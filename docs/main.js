cfbdf7c313224ee34088eb1da0c13669c0935cd7
Linh Phan <phanthuclinh@gmail.com>
2026-01-07 16:38:27 -0800

    adding sticky nav bar with blurr effect as you scroll

---
+++ docs/main.js
@@ -0,0 +1,11 @@
+// Nav scroll effect
+const nav = document.querySelector('.nav');
+if (nav) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
+}
